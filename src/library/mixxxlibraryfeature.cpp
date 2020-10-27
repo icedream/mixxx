7dc7ba30beabf54538f923e2b892548aae9bbd5a
Adam Szmigin <smidge@xsco.net>
2020-10-27 23:59:45 +0000

    All djinterop exceptions caught, further cleanup

--- src/library/mixxxlibraryfeature.cpp
+++ src/library/mixxxlibraryfeature.cpp
@@ -117,7 +117,7 @@ MixxxLibraryFeature::MixxxLibraryFeature(Library* pLibrary,
     connect(m_pExportLibraryAction.get(),
             &QAction::triggered,
             this,
-            &MixxxLibraryFeature::slotExportLibrary);
+            &MixxxLibraryFeature::exportLibrary);
 #endif
 }
 
@@ -215,9 +215,5 @@ void MixxxLibraryFeature::onRightClick(const QPoint& globalPos) {
     QMenu menu(m_pSidebarWidget);
     menu.addAction(m_pExportLibraryAction.get());
     menu.exec(globalPos);
-}
-
-void MixxxLibraryFeature::slotExportLibrary() {
-    emit exportLibrary();
 }
 #endif
